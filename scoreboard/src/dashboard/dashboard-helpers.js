1655c0244a617db37c1df3a1c5b224b1232e61f6
kjohna <43828763+kjohna@users.noreply.github.com>
2019-03-05 16:21:18 -0700

    restes balls and strikes when strikes reaches 3

--- scoreboard/src/dashboard/dashboard-helpers.js
+++ scoreboard/src/dashboard/dashboard-helpers.js
@@ -1,11 +1,14 @@
 export function strikeCounter(state) {
   let updatedStrikes = state.strikes;
+  let updatedBalls = state.balls;
   updatedStrikes ++;
-  if(updatedStrikes > 3) {
+  if(updatedStrikes > 2) {
     updatedStrikes = 0;
+    updatedBalls = 0;
   }
   return {
     ...state,
-    strikes: updatedStrikes
+    strikes: updatedStrikes,
+    balls: updatedBalls
   }
 }
--- scoreboard/src/dashboard/dashboard-helpers.spec.js
+++ scoreboard/src/dashboard/dashboard-helpers.spec.js
@@ -1,4 +1,4 @@
-const { strikeCounter } = require('./dashboard-helpers.js');
+const { strikeCounter, ballCounter } = require('./dashboard-helpers.js');
 
 describe('dashboard-helpers.js', () => {
   describe('countStrike()', () => {
@@ -8,6 +8,18 @@ describe('dashboard-helpers.js', () => {
         strikes: 0,
       }
       expect(strikeCounter(state).strikes).toBe(1);
+    })
+
+    it('should reset balls and strikes when strikes reaches 3', () => {
+      const startState = {
+        balls: 3,
+        strikes: 2,
+      }
+      const finalState = {
+        balls: 0,
+        strikes: 0,
+      }
+      expect(strikeCounter(startState)).toEqual(finalState);
     })
   })
 });
